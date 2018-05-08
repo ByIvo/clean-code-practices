13d3353bcb9f1314b53dd717e4e079136f8ea583
Anderson Matheus Lemke <andersonmatheus.lemke@gmail.com>
2018-05-08 20:56:49 -0300

    Creating palindrome checker

--- src/main/java/clean/code/problems/Problem4.java
+++ src/main/java/clean/code/problems/Problem4.java
@@ -1,10 +1,8 @@
 package clean.code.problems;
 
+public class Problem4 {
 
-public class Problem4
-{
-	public static void main(String[] args)
-	{
-		
-	}
+  public static void main(String[] args) {
+    
+  }
 }

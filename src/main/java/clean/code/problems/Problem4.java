2d9c019d05259a27cac04247ccc80ab8791b59dd
Anderson Matheus Lemke <andersonmatheus.lemke@gmail.com>
2018-05-08 20:56:49 -0300

    Creating palindrome checker

--- src/main/java/clean/code/problems/Problem4.java
+++ src/main/java/clean/code/problems/Problem4.java
@@ -1,20 +1,10 @@
 package clean.code.problems;
 
-import java.util.Scanner;
 
-import clean.code.problems.solutions.LargestPalindromeDiscover;
+public class Problem4 {
 
-public class Problem4
-{
-	public static void main(String[] args)
-	{
-		final Scanner scanner = new Scanner(System.in);
-		long digits = scanner.nextLong();
-		
-		LargestPalindromeDiscover largestPalindromeDiscover = new LargestPalindromeDiscover();
-		Long largestPalindromeFound = largestPalindromeDiscover.withNDigitis(digits);
-		
-		System.out.println(largestPalindromeFound);
-		scanner.close();
-	}
+  public static void main(String[] args) {
+    
+  }
+
 }

8c5da5bc249278bee89336404548203dbd479432
Felipe Santolin <Felipe Santolin>
2018-05-09 11:09:53 -0300

    problema 4 identação

--- src/main/java/clean/code/problems/Problem4.java
+++ src/main/java/clean/code/problems/Problem4.java
@@ -1,34 +1,34 @@
 package clean.code.problems;
 
-public class Problem4
-{
-	public static void main(String[] args)
-	{
-		resolveTheProblem();
-	}
+public class Problem4 {
 
-	public static void resolveTheProblem(){
-		int largest3DigitsNumber = 999;
-		
-		int largestPalindrome = 0;
 
-		for (int i = 1; i <= largest3DigitsNumber; i++) {
-			for (int j = 1; j <= largest3DigitsNumber ; j++) {
-				int result = i*j;
-				if(isPalindrome(result) && result > largestPalindrome){
-					largestPalindrome = result;
-				}
-			}
-		}
-		System.out.println("Largest Palindrome from 3 digits: "+largestPalindrome);
-	}
+  public static void main(String[] args) {
+    resolveTheProblem();
+  }
 
-	private static boolean isPalindrome(int result) {
-		String resultString = String.valueOf(result);
-		String resultReverse = new StringBuffer(resultString).reverse().toString();
-		if(resultReverse.equals(resultString)){
-			return true;
-		}
-		return false;
-	}
+  public static void resolveTheProblem() {
+    int largest3DigitsNumber = 999;
+
+    int largestPalindrome = 0;
+
+    for (int i = 1; i <= largest3DigitsNumber; i++) {
+      for (int j = 1; j <= largest3DigitsNumber; j++) {
+        int result = i * j;
+        if (isPalindrome(result) && result > largestPalindrome) {
+          largestPalindrome = result;
+        }
+      }
+    }
+    System.out.println("Largest Palindrome from 3 digits: " + largestPalindrome);
+  }
+
+  private static boolean isPalindrome(int result) {
+    String resultString = String.valueOf(result);
+    String resultReverse = new StringBuffer(resultString).reverse().toString();
+    if (resultReverse.equals(resultString)) {
+      return true;
+    }
+    return false;
+  }
 }

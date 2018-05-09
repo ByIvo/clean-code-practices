469e6988a3ffc6548703ae96c4fb694c6d42a698
Anderson Matheus Lemke <andersonmatheus.lemke@gmail.com>
2018-05-09 00:30:50 -0300

    Problem 4 solution

--- src/main/java/clean/code/problems/Problem4.java
+++ src/main/java/clean/code/problems/Problem4.java
@@ -1,8 +1,14 @@
 package clean.code.problems;
 
+import clean.code.solutions.problem4.LargestPalindromeFinderForProductThreeDigit;
+
 public class Problem4 {
 
   public static void main(String[] args) {
-    
+    LargestPalindromeFinderForProductThreeDigit largestPalindromeFinder = new LargestPalindromeFinderForProductThreeDigit();
+    int largestPalindromeForThreeDigits = largestPalindromeFinder.find();
+
+    System.out.println(largestPalindromeForThreeDigits);
   }
+
 }

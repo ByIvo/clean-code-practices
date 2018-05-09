bf65039370deef8c560836a81219d68b0c3faa41
Anderson Matheus Lemke <andersonmatheus.lemke@gmail.com>
2018-05-09 00:30:50 -0300

    Problem 4 solution

---
+++ src/main/java/clean/code/solutions/problem4/LargestPalindromeFinderForProductThreeDigit.java
@@ -0,0 +1,34 @@
+package clean.code.solutions.problem4;
+
+public class LargestPalindromeFinderForProductThreeDigit {
+
+  private static final int MAXIMUM_THREE_DIGIT_NUMBER = 999;
+  private static final int MINIMUM_THREE_DIGIT_NUMBER = 100;
+  private PalindromeChecker palindromeChecker = new PalindromeChecker();
+
+  public int find() {
+    int largestProduct = -1;
+    int product;
+
+    for (int firstMultiplier = MAXIMUM_THREE_DIGIT_NUMBER; firstMultiplier > MINIMUM_THREE_DIGIT_NUMBER; firstMultiplier--) {
+      for (int secondMultiplier = firstMultiplier; secondMultiplier > MINIMUM_THREE_DIGIT_NUMBER; secondMultiplier--) {
+        product = firstMultiplier * secondMultiplier;
+
+        if (isPalindromeAndGreaterThatCurrent(largestProduct, product)) {
+          largestProduct = product;
+        }
+      }
+    }
+
+    return largestProduct;
+  }
+
+  private boolean isPalindromeAndGreaterThatCurrent(int largestProduct, int product) {
+    return isPalindrome(product) && product > largestProduct;
+  }
+
+  private boolean isPalindrome(int product) {
+    return palindromeChecker.isPalindrome(Integer.toString(product));
+  }
+
+}
